b98244101b1660329583e485701a0a6a0b8ae8ed
Rao (Katie) Xu <dnait@users.noreply.github.com>
2017-07-18 16:29:23 -0400

    Update removeStopWords.java

--- removeStopWords.java
+++ removeStopWords.java
@@ -6,31 +6,24 @@ import java.io.IOException;
 import java.io.StringReader;
 
 import org.apache.lucene.analysis.CharArraySet;
-
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.StopAnalyzer;
-
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.util.AttributeFactory;
 
 public class Stopwords_remove {
-    @SuppressWarnings("resource")
+
 	public String removeStopWords(String string) throws IOException {
         
         StandardTokenizer tokenizer = new StandardTokenizer();
-        
-        TokenStream tokenStream = new StandardTokenizer();
-        
+               
         tokenizer.setReader(new StringReader(string));
         
         StringBuilder sb = new StringBuilder();
         
-        tokenStream = new StopFilter(tokenizer, new CharArraySet(StopAnalyzer.ENGLISH_STOP_WORDS_SET, true));
-       // tokenStream.close();
+        TokenStream tokenStream = new StopFilter(tokenizer, new CharArraySet(StopAnalyzer.ENGLISH_STOP_WORDS_SET, true));
         tokenizer.reset();
-        
         CharTermAttribute token = tokenizer.getAttribute(CharTermAttribute.class);
 
 
@@ -42,7 +35,8 @@ public class Stopwords_remove {
             }
             sb.append(token.toString());
         }
-        
+        tokenStream.close();
+		tokenizer.close();
         return sb.toString();
     }
 
