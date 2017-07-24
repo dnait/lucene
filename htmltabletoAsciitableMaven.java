58590f038d2671e37742373232095bef979e866f
Rao (Katie) Xu <dnait@users.noreply.github.com>
2017-07-23 21:35:31 -0400

    Add Object creation
    Add Object creation park when pass in multiple Strings

--- htmltabletoAsciitableMaven.java
+++ htmltabletoAsciitableMaven.java
@@ -15,7 +15,12 @@ public class TableToasciitableDemo {
 		array[1] = "row 1 col 1";
 		at.addRow("row 1 col 1", "row 2 col 2");
 		//When reading elements from other methods
-		at.addRow(new Object[]{array[0],array[0]});
+		Object[] rowObject = new Object[array.length];
+		for (int i = 0; i < array.length; i++) {
+			rowObject[i] = array[i];
+		}
+		at.addRow(rowObject);
+//		at.addRow(new Object[]{array[0],array[0]});
 		at.addRule();
 		at.addRow("row 2 col 1row 2 col 1row 2 col 1row 2 col 1row 2 col 1", "row 2 col 2");
 		at.addRule();
